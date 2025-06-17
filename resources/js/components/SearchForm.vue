a67dc4ed33d181ba197577b52c09dabbe826d91a
beybora <bora.b.sarikaya@gmail.com>
2025-06-17 18:47:19 +0200

    feat(frontend): create SearchForm component and add it to AppSidebarHeader

---
+++ resources/js/components/SearchForm.vue
@@ -0,0 +1,16 @@
+<template>
+     <form class="flex w-full max-w-md items-center gap-3 px-4">
+        <Input v-model="searchQuery" placeholder="Search" class="flex-1" @keyup.enter="submitSearch" />
+    </form>
+</template>
+
+<script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue';
+import { ref } from 'vue';
+
+const searchQuery = ref('');
+
+const submitSearch = () => {
+    console.log('Search:', searchQuery.value);
+};
+</script>
